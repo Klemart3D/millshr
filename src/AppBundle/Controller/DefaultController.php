c2d291a844a41d468e2f6cd588dcf2b9a7da6e81
Klément Grisel <Klems@Klem-MBP.local>
2016-03-08 11:42:18 +0100

    adding yo

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -10,12 +11,11 @@ class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+	 * @Method({"GET"})
      */
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ]);
+        return $this->render('default/index.html.twig');
     }
 }
